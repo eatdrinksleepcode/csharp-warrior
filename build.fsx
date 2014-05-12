a51a5b0b1961ba5a584f276d476f238bfb06b16c
David Nelson <eatdrinksleepcode@gmail.com>
2014-05-11 23:33:49 -0400

    [David] Initial build script

---
+++ build.fsx
@@ -0,0 +1,13 @@
+﻿// include Fake lib
+#r @"packages/FAKE.2.15.5/tools/Nuget.Core.dll"
+#r @"packages/FAKE.2.15.5/tools/FakeLib.dll"
+open Fake
+
+Target "BuildApp" (fun _ ->
+    !! "Source/CSharpWarrior.Web.Host/CSharpWarrior.Web.Host.csproj"
+        |> MSBuildRelease "" "Build"
+        |> Log "AppBuild-Output: "
+)
+
+// start build
+RunTargetOrDefault "BuildApp"
