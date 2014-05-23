e1f2989b49a94aa9ea5b63cc843bd583d2c0f066
David Nelson <eatdrinksleepcode@gmail.com>
2014-05-23 19:07:33 -0400

    [David] Add Debuggable attribute to Server project

---
+++ Source/CSharpWarrior.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics;
+
+// XBuild doesn't add this, but Nancy uses it to detect debug mode (for view caching etc)
+#if DEBUG
+[Debuggable (DebuggableAttribute.DebuggingModes.Default)]
+#endif
